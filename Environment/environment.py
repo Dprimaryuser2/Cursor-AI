cfeb32ae4e9f39cea4c4cd78e909db5efc17536c
Samarth Math <samarth.math@indexnine.com>
2024-07-04 18:30:08 +0530

    Updated Changes

--- Environment/environment.py
+++ Environment/environment.py
@@ -1,6 +1,5 @@
 admin_console_url = "https://staging.legacyconsole.gozwing.com/"
 pos_url = " https://staging.app.gozwing.com/"
-browser = 'edge'
-# browser = 'chrome'
+browser = 'chrome'
 window_height = 1920
 window_width = 1080
